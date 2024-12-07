9a62ddd87298bdf26dc276dafc0d433d8863df0c
Sergei Tikhomirov <s.tikhomirov@arenadata.io>
2024-12-07 15:59:49 +0300

    fixed tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,15 +1,34 @@
-from llamba_framework.chatmodels.chat_model import AbstractChatModel
+from llamba_library.bioage_model import BioAgeModel
+import numpy as np
+import pandas as pd
+import torch
 import unittest
 
 class TestAnalyzeFunction(unittest.TestCase):
     def test_query(self):        
-        class DummyChatModel(AbstractChatModel): 
-            def query(self, prompt, timeout):
-                return "Nice question!"
-        chat_model = DummyChatModel()
-        prompt = f'What is C-reactive protein? What does an increased level of C-reactive protein mean?'
-        res = chat_model.query(prompt=prompt, timeout=60)
-        self.assertEqual(str.strip(res), "Nice question!")
+        # Prepare data to analyze
+        np.random.seed(0)
+        torch.manual_seed(0)
+        num_features = 10
+        features = np.random.randint(low=1, high=150, size=num_features).astype(np.float32)
+        age =  np.random.randint(low=10, high=90)
 
+        data = pd.DataFrame([{f'Feature_{i}' : features[i] for i in range(num_features)}])
+
+        # Prepare a BioAge model
+        class DummyBioAgeModel(torch.nn.Module): 
+            def __init__(self): 
+                super(DummyBioAgeModel, self).__init__()
+                self.linear = torch.nn.Linear(10, 1)
+
+            def forward(self, x):
+                x = self.linear(x)
+                return abs(x)
+            
+        model = DummyBioAgeModel()
+        bioage_model = BioAgeModel(model)
+        res = bioage_model.inference(data, torch.device('cpu'))
+        self.assertIsInstance(res[0], np.float32)
+            
 if __name__ == '__main__':
     unittest.main()
